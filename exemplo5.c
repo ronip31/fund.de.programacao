660cc8a47813bc0a7ab754e65e4ca5e243fb0c4f
Roni Pereira <roni.pereira31@gmail.com>
2022-08-23 00:16:22 -0300

    teste2

--- exemplo5.c
+++ exemplo5.c
@@ -1,6 +1,7 @@
 // início
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include "stdio.h"
+#include "stdlib.h"
 
 int main(void){
 	// variáveis
---
+++ hello.c
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "header.h"
+
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+}
