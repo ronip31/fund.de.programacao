558aaaa937412fe6b7c54cc673fff24ea8a4cf3f
Roni Pereira <roni.pereira31@gmail.com>
2022-08-24 00:00:41 -0300

    exercício if else

---
+++ ordemcrescente.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Faça um fluxograma para resolver o seguinte problema, dado três números coloquem em ordem crescente.
+
+int main(){
+
+float a, b, c;
+
+    printf(" Informe primeiro numero: \n");
+    scanf("%f", &a);
+    fflush(stdin);
+    printf("Informe segundo numero: \n");
+    scanf("%f", &b);
+    printf("Informe terceiro numero \n");
+    scanf("%f", &c);
+    fflush(stdin);
+    if (a < b && a < c && b < c)
+    {
+        printf(" A < B < C");
+    } else if (b < c && b < a && c < a)
+    {
+        printf(" B < C < A");
+    } else if (c < b && b < a)
+    {
+        printf(" C < B < A");
+    } else {
+        printf("C < A < B");
+    }
+    
+    
+      
+
+
+
+
+
+
+
+
+
+    return 0;
+}
